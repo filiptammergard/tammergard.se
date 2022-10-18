db50b0885cef960bcb28f213ecdca100e084ed20
Filip Tammergård <filip@tammergard.se>
2022-10-18 15:12:58 +0200

    fix: remove semi colons

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -6,11 +6,11 @@ export function Presentation() {
 	const weeks = useWeeksAgo(new Date("1994-08-06T10:00"))
 	return (
 		<CodeBlock language="ts" title="Presentation">
-			{`const name = "Filip Tammergård";
-const location = "Uppsala, Sweden 🇸🇪";
-let ageInWeeks = ${weeks};
+			{`const name = "Filip Tammergård"
+const location = "Uppsala, Sweden 🇸🇪"
+let ageInWeeks = ${weeks}
 
-makePresentation({ name, location, age: weeksToYears(ageInWeeks) });`}
+makePresentation({ name, location, age: weeksToYears(ageInWeeks) })`}
 		</CodeBlock>
 	)
 }
