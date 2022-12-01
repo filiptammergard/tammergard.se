83620d38c9e50bf27db8694f21d3b2df58aa897f
Filip Tammergård <filip@tammergard.se>
2022-12-01 09:54:01 +0100

    feat: publish border radius blog post

---
+++ blog/2022-12-01-border-radius-ellipses/Shape.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./styles.module.css"
+
+export function Shape() {
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.shape}></div>
+		</div>
+	)
+}
